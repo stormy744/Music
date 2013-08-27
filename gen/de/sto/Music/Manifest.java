44139a29ed23eea990481aa3c4e478060c9dc7ef
stormy744 <s.stolp@europe.com>
2013-08-27 19:27:27 +0200

    First commit

---
+++ gen/de/sto/Music/Manifest.java
@@ -0,0 +1,7 @@
+/*___Generated_by_IDEA___*/
+
+package de.sto.Music;
+
+/* This stub is for using by IDE only. It is NOT the Manifest class actually packed into APK */
+public final class Manifest {
+}
---
+++ src/de/sto/Music/MainScreen.java
@@ -0,0 +1,89 @@
+package de.sto.Music;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.DragEvent;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+public class MainScreen extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        makeFullscreen();
+        setContentView(R.layout.main);
+
+        buttonsUpDown();
+
+    }
+
+    private void buttonsUpDown() {
+        final ImageButton buttonUp = (ImageButton)findViewById(R.id.upButton);
+        final ImageButton buttonDown = (ImageButton)findViewById(R.id.downButton);
+        final LinearLayout nowPlayingBar = (LinearLayout)findViewById(R.id.nowPlayingBar);
+
+        if(buttonDown.getVisibility() == View.INVISIBLE){
+            buttonUp.setVisibility(View.VISIBLE);
+        } else {
+            buttonUp.setVisibility(View.VISIBLE);
+            buttonDown.setVisibility(View.INVISIBLE);
+        }
+
+        buttonUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeLayoutWeight(nowPlayingBar, 100, 3);
+                nowPlayingBar.setVisibility(View.INVISIBLE);
+                changeVisibleButton(buttonUp,buttonDown);
+            }
+        });
+
+        buttonDown.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeLayoutWeight(nowPlayingBar, 10, 3);
+                nowPlayingBar.setVisibility(View.VISIBLE);
+                changeVisibleButton(buttonUp,buttonDown);
+            }
+        });
+    }
+
+    //changes visible button
+    private void changeVisibleButton(ImageButton btn1, ImageButton btn2) {
+            if(btn1.getVisibility() == View.VISIBLE){
+                btn1.setVisibility(View.INVISIBLE);
+                btn2.setVisibility(View.VISIBLE);
+            } else {
+                btn1.setVisibility(View.VISIBLE);
+                btn2.setVisibility(View.INVISIBLE);
+            }
+    }
+
+    //changes layout weight to given value
+    private void changeLayoutWeight(LinearLayout layout, float value, int modeAddSubIs) {
+        //modeAddSubIs: 1 -> Add, 2 -> Sub, 0/3+ -> set to value
+        LinearLayout.LayoutParams lParams = (LinearLayout.LayoutParams) layout.getLayoutParams();
+        if(modeAddSubIs == 1) {
+            lParams.weight += value;
+        }else if(modeAddSubIs == 2){
+            lParams.weight -= value;
+        }else{
+            lParams.weight = value;
+        }
+        layout.setLayoutParams(lParams);
+    }
+
+    //Displays the App in Fullscreen-Mode
+    public void makeFullscreen(){
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    }
+}
